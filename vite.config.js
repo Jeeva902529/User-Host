98f7dcd3830ad68f0710f84900135d2d828d33cc
R jeevanantham <106539570+Jeeva902529@users.noreply.github.com>
2025-05-05 01:54:41 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   build: {
-    outDir: 'dist', // Default for Vite, but explicitly stated for clarity
-    chunkSizeWarningLimit: 1000, // Adjust to suppress chunk size warnings
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
   },
 })
