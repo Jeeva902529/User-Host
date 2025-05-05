c1404919356cf0593248e4e6c442cbfcee725032
R jeevanantham <106539570+Jeeva902529@users.noreply.github.com>
2025-05-04 23:27:21 -0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     setIsLoading(true);
   
     try {
-      const response = await fetch("http://localhost:5000/api/auth", {
+     const response = await fetch("https://back-end-res-6emf.onrender.com/api/auth", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, password, tableNumber }),
